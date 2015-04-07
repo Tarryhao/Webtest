53780df3795c4e69e2138b8e287b6542148f050d
Tarryhao <tang61989978@126.com>
2015-04-07 11:49:04 +0800

    A.07

--- src/eg/WebATOchecklogin.java
+++ src/eg/WebATOchecklogin.java
@@ -7,17 +7,16 @@ import java.sql.Statement;
 public class WebATOchecklogin {
 
 		public  int checkid(String id,String pwd) throws Exception {
-			   
-			   Class.forName("oracle.jdbc.driver.OracleDriver");
-				// 2.得到连接
-				// DriverManager.getConnection("连接协议", "用户名", "密码");
+			    //1.加载驱动
+			    Class.forName("oracle.jdbc.driver.OracleDriver");
+				//2.DriverManager.getConnection("连接协议", "用户名", "密码");
 				Connection conn = DriverManager.getConnection(
 						"jdbc:oracle:thin:@127.0.0.1:1521:ORCL", "Study_user", "123456");
-
-				// 3.得到SQL流
+				//3.得到SQL流
 				Statement statement = conn.createStatement();
 				
-				String sql = "select * from login where id='"+id+"'";// 要执行的sql语句	
+				// 要执行的sql语句
+				String sql = "select * from login where id='"+id+"'";	
 				ResultSet exe = statement.executeQuery(sql);
 				
 				if (exe.next()) {
