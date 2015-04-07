514ba896cb084e8c7bd5706557340ae091a30eed
Tarryhao <tang61989978@126.com>
2015-04-07 11:29:43 +0800

    A.07
    使用预处理方式处理sql语句

--- src/eg/WebATOchange.java
+++ src/eg/WebATOchange.java
@@ -1,26 +1,30 @@
 package eg;
-
+//该类用于修改用户数据库上的信息
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 public class WebATOchange {
 
 		public  void regis(String id,String pwd,String phone,String qq,String sex,String like) throws Exception {
-
-			   Class.forName("oracle.jdbc.driver.OracleDriver");
-				// 2.得到连接
+               	//加载驱动
+				Class.forName("oracle.jdbc.driver.OracleDriver");
 				// DriverManager.getConnection("连接协议", "用户名", "密码");
 				Connection conn2 = DriverManager.getConnection(
 						"jdbc:oracle:thin:@127.0.0.1:1521:ORCL", "Study_user", "123456");
-
-				// 3.得到SQL流
-				Statement statement = conn2.createStatement();
-				String sql2 = "UPDATE login   SET pwd ='"+pwd+"',phone='"+phone+"',qq='"+qq+"',sex='"+sex+"',glad='"+like+"' WHERE id='"+id+"'";// 要执行的sql语句
-				System.out.println(sql2);
-		System.out.println("修改成功");
-					statement.executeUpdate(sql2);
-					statement.close();
-					conn2.close();
+				String sql="UPDATE login   SET pwd =?,phone=?,qq=?,sex=?,glad=? WHERE id=?";
+				//sql语句预处理
+				PreparedStatement ps=conn2.prepareStatement(sql);
+				//给？传值
+				ps.setString(1, pwd);
+				ps.setString(2, phone);
+				ps.setString(3, qq);
+				ps.setString(4, sex);
+				ps.setString(5, like);
+				ps.setString(6, id);
+				int number=ps.executeUpdate();
+				System.out.println("数据库"+number+"行受影响！");
+				ps.close();
+				conn2.close();
 				
 				}
 				
